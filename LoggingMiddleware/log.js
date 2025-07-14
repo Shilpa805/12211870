a99a91b4de7fe54da80ecbe498d4f850ba0d8c39
shilpaak805 <shilpaak805@gmail.com>
2025-07-14 10:29:36 +0530

    added reusable logging middleware function

---
+++ LoggingMiddleware/log.js
@@ -0,0 +1,25 @@
+export async function Log(stack, level, pkg, message, token) {
+  const url = "http://29.244.56.144/evaluation-service/logs";
+
+  const payload = {
+    stack: stack,     
+    level: level,     
+    package: pkg,     
+    message: message  
+  };
+
+  const res = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${token}`
+    },
+    body: JSON.stringify(payload)
+  });
+
+  if (!res.ok) {
+    console.error("Logging failed:", res.statusText);
+  } else {
+    console.log("Log created successfully");
+  }
+}
